1b080de4073caafb6b2d859337651e3a2464b8ee
lupingtao <lupingtao@youmi.net>
2017-06-30 16:57:47 +0800

    随便写点

--- things.py
+++ things.py
@@ -1,10 +1,13 @@
 # coding=utf8
 import falcon
+import redis
 
+r = redis.Redis(host='localhost', port=6379, db=3)
 
 class ThingsResource(object):
 
     def on_get(self, req, resp):
+        r.incr('fastapi:receive:success')
         resp.status = falcon.HTTP_200
         resp.body = 'Hello World!'
 
